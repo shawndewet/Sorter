1f2f7a072232b435af057e612fb8d5863a77d5d8
Shawn de Wet <shawndewet@codewell.co.za>
2021-01-26 17:21:25 +0200

    Added Console input.

--- Sorter/Program.cs
+++ Sorter/Program.cs
@@ -6,7 +6,17 @@ namespace Sorter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sorter = new SorterLibrary.Sorter();
+            Console.WriteLine("Hello! Please enter the one-liner string you would like to sort: (leave input blank to quit)");
+            string stringToSort = Console.ReadLine();
+            while (!string.IsNullOrEmpty(stringToSort))
+            {
+                Console.WriteLine($"Sorted: {sorter.SortHandCoded(stringToSort)}");
+                Console.WriteLine("");
+                Console.WriteLine("Please enter the one-liner string you would like to sort: (leave input blank to quit)");
+                stringToSort = Console.ReadLine();
+            }
+            Console.WriteLine("Bye.");
         }
     }
 }
