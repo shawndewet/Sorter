20bdf303b59b519d5a64584d8dc6cd024845c116
Shawn de Wet <shawndewet@codewell.co.za>
2021-01-26 17:08:52 +0200

    Implemented Insertion sort. All tests pass

--- SorterLibrary/Sorter.cs
+++ SorterLibrary/Sorter.cs
@@ -12,9 +12,29 @@ namespace SorterLibrary
             return string.Concat(sortedStringArray);
         }
 
+        /// <summary>
+        /// This uses the Insertion Sort algorithm, which my tests have shown to be the quickest,
+        ///   although I know QuickSort is theoretically supposed to be quicker.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>The input string sorted ascending</returns>
         public string SortHandCoded(string input)
         {
-            throw new NotImplementedException();
+            var strippedString = StripPunctuation(input.ToLower());
+            var charArray = strippedString.ToCharArray();
+
+            for (int i = 1; i < charArray.Length; i++)
+            {
+                var temp = charArray[i];
+                var j = i - 1;
+                while (j >= 0 && charArray[j] > temp)
+                {
+                    charArray[j + 1] = charArray[j];
+                    j--;
+                }
+                charArray[j + 1] = temp;
+            }
+            return string.Concat(charArray);
         }
 
         private string StripPunctuation(string input)
--- SorterTests/SorterTests.cs
+++ SorterTests/SorterTests.cs
@@ -17,6 +17,7 @@ namespace SorterTests
 
         [Theory()]
         [InlineData("Shawn", "ahnsw")]
+        [InlineData("Contrary to popular belief, the pink unicorn flies east.", "aaabcceeeeeffhiiiiklllnnnnooooppprrrrssttttuuy")]
         public void TestHandCodedSort(string input, string expected)
         {
             var testTarget = new SorterLibrary.Sorter();
